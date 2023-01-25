e6c3615b2e7a92096fc4fd526b7f7f567edeeaca
Martin Roštejnský <martin.rostejnsky@email.cz>
2023-01-25 09:35:17 +0100

    Add project files.

---
+++ ExtensionMethods/ListExtended.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ListExtended
+    {
+        public static List<string> ShorterThan(this List<string> list, int threshold) {
+            List<string> result = new List<string>();
+            foreach (string s in list) {
+                if (s.Length < threshold) {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using ExtensionMethods;
+
+List<string> _extensions = new List<string>();
+_extensions.Add("nevim");
+_extensions.Add("jaj");
+_extensions.Add("pico");
+_extensions.Add("au");
+
+List<string> tonevim = _extensions.ShorterThan(4);
+foreach (string extension in tonevim)
+{
+    Console.WriteLine(extension);
+}
