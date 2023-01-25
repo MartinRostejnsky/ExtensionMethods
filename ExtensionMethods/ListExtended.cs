8a7d5be11d86201ba086cd62d926bf674c5e1ce6
Martin Roštejnský <martin.rostejnsky@email.cz>
2023-01-25 09:50:31 +0100

    string progress

--- ExtensionMethods/ListExtended.cs
+++ ExtensionMethods/ListExtended.cs
@@ -17,5 +17,52 @@ namespace ExtensionMethods
             }
             return result;
         }
+        public static List<string> Contains(this List<string> list, string x)
+        {
+            List<string> result = new List<string>();
+            foreach (string s in list)
+            {
+                if (s.Contains(x))
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+
+        public static List<string> LengthBetween(this List<string> list, int x, int y)
+        {
+            List<string> result = new List<string>();
+            foreach (string s in list)
+            {
+                if (s.Length <= y & s.Length >= x)
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
+
+        public static List<string> ContainsUpperLetter(this List<string> list)
+        {
+            List<string> result = new List<string>();
+            foreach (string s in list)
+            {
+                bool ContainsUpper = false;
+                foreach (char x in s)
+                {
+                    if (char.IsUpper(x))
+                    {
+                        ContainsUpper = true;
+                        break;
+                    }
+                }
+                if (ContainsUpper)
+                {
+                    result.Add(s);
+                }
+            }
+            return result;
+        }
     }
 }
--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -1,13 +1,20 @@
 ﻿using ExtensionMethods;
 
 List<string> _extensions = new List<string>();
-_extensions.Add("nevim");
+_extensions.Add("Nevim");
 _extensions.Add("jaj");
-_extensions.Add("pico");
-_extensions.Add("au");
+_extensions.Add("pIco");
+_extensions.Add("aU");
 
 List<string> tonevim = _extensions.ShorterThan(4);
 foreach (string extension in tonevim)
+{
+    Console.WriteLine(extension);
+}
+Console.WriteLine();
+
+List<string> tonevim2 = _extensions.ContainsUpperLetter();
+foreach (string extension in tonevim2)
 {
     Console.WriteLine(extension);
 }
